ac3786a4e689e0a8bd96de8f55e83daf2bb0d175
Kaige <millerkaige@gmail.com>
2022-07-16 18:45:56 -0700

    Add project files.

---
+++ Coffee Shop Registration/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Coffee_Shop_Registration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffee_Shop_Registration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult RegistrationSubmit(CoffeeUser user)
+        {
+            return View("RegistrationSubmit", user);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Coffee Shop Registration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coffee_Shop_Registration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
